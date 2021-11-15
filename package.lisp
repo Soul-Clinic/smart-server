6edc8584cd6a0d0435d6e42bdbed3426b42c9865
Can <can@celwk.com>
2021-11-15 20:52:27 +0800

    smart-server package

--- package.lisp
+++ package.lisp
@@ -1,12 +1,40 @@
 (in-package :cl)
 
-(defpackage :smart-server/test
-  (:nicknames :sst)
-  (:use :cl
-        :celwk
-        :hunchentoot
-        :com.gigamonkeys.json
-        :smart-server)
-  (:export
-   #:query-value-list
-   #:mysql))
+(defpackage :smart-server	;; TODO: Change to neverland ?
+  (:nicknames :server :smart :ss :neverland)
+  (:use #:cl
+        #:celwk
+        #:hunchentoot
+        #:hunchensocket
+        #:cl-ppcre
+        #:do-urlencode
+        #:flexi-streams
+        #:com.gigamonkeys.json)
+  (:export #:*neverland*
+           #:*smart-acceptors*
+           #:*last-request*
+           #:try-fetch-location
+           #:express-status-json 
+           #:create-smartor
+           #:create-land
+           #:smart+
+           #:smart=>
+           #:smart-acceptor
+           #:land+
+           #:land=>
+           #:@next
+           #:@continue
+           #:land-acceptor
+           #:ip-location
+           #:delete-smart-routes
+           #:connect-postgres
+           #:disconnect-postgres
+           #:pooled-query
+           #:query-arrays
+           #:query-json
+           #:<-sql
+           #:<=sql
+           #:sql->
+           #:sql=>
+           #:*query-type*))
+
